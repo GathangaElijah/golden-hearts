67c84b57c5c8810a4e65b517c227a6a68f00cdf9
GathangaElijah <elyg3672@gmail.com>
2025-06-10 22:10:43 +0300

    feat: added a template code

--- backend/handlers/admin.go
+++ backend/handlers/admin.go
@@ -2,11 +2,13 @@ package handlers
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func AdminHandler() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("DonationsHandler called for path: %s", r.URL.Path)
 		fmt.Fprintf(w, "Welcome to the Admin Page!")
 	})
 }
