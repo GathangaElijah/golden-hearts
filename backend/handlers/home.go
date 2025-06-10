bd73068a725c0c0e574c64a607a60360c6620ef7
GathangaElijah <elyg3672@gmail.com>
2025-06-10 22:12:31 +0300

    refactor: added a template code for the function

--- backend/handlers/home.go
+++ backend/handlers/home.go
@@ -2,11 +2,13 @@ package handlers
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func HomeHandler() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("DonationsHandler called for path: %s", r.URL.Path)
 		fmt.Fprintf(w, "Welcome to the Home Page!")
 	})
 }
