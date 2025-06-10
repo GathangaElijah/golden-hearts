d731e13548035754587df2f1641f8735467a33f1
GathangaElijah <elyg3672@gmail.com>
2025-06-10 21:51:04 +0300

    refctor: add a projects sample file

--- backend/handlers/projects.go
+++ backend/handlers/projects.go
@@ -2,11 +2,44 @@ package handlers
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"path"
 )
+
+type ProjectData struct {
+	Id          string `json:"id"`
+	Title       string `json:"project-title"`
+	Description string `json:"project-description"`
+	Amount      string `json:"contribution-target"`
+}
 
 func ProjectsHandler() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Welcome to the Projects Page!")
+
+		log.Printf("Projects Handler called for path: %s", r.URL.Path)
+
+		if r.Method != http.MethodGet {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		cwd, err := os.Getwd()
+		if err != nil {
+			fmt.Printf("Problem opening current working dir\n%v", err)
+			http.Error(w, "Internal error: path resolution", http.StatusInternalServerError)
+			return
+		}
+		projectsFilePath := path.Join(cwd, "backend", "data", "projects.json")
+
+		fileContent, err := os.ReadFile(projectsFilePath)
+		if err != nil {
+			http.Error(w, "Unable to read projects data", http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(fileContent)
 	})
 }
